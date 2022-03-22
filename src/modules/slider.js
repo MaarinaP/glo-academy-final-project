a4bc4f44c03bd2aa1710cfddd182e457cba3a12e
Marina Pavlova <marinap.learning@gmail.com>
2022-03-22 16:05:19 +0000

    Add slider module to move carousels

--- src/modules/slider.js
+++ src/modules/slider.js
@@ -3,72 +3,62 @@
 const slider = () => {
     const body = document.querySelector("body");
     const benefitBlock = document.querySelectorAll(".benefits__item");
-    const benefitArrows = document.querySelectorAll(".benefits__arrow");
     const serviceBlock = document.querySelectorAll(".service-block");
 
-    //create arrays for visible and hidden elements
     //numberLarge - how many elements to show on the screen for large screen with screenWidthLarge width and higher
+    //numberSmall - how many elements to show on the screen for screen width lower then screenWidthLarge
     const visibleSlider = (domElement, numberLarge, numberSmall, screenWidthLarge = 576) => {
-        const array = [];
-        let visibleArr = [];
-        let hiddenArr = [];
-
         domElement.forEach((item) => {
             item.style.display = "none";
-            array.push(item);
         });
 
         if (screen.width >= screenWidthLarge) {
-            visibleArr = array.slice(0, numberLarge);
-            hiddenArr = array.slice(numberLarge);
+            for (let i = 0; i < numberLarge; i++) {
+                domElement[i].style.display = "block";
+            }
         } else {
-            visibleArr = array.slice(0, numberSmall);
-            hiddenArr = array.slice(numberSmall);
+            for (let i = 0; i < numberSmall; i++) {
+                domElement[i].style.display = "block";
+            }
         }
-
-        visibleArr.forEach((item) => {
-            item.style.display = "block";
-        });
-
-        return [visibleArr, hiddenArr];
     };
 
     visibleSlider(benefitBlock, 3, 2);
     visibleSlider(serviceBlock, 2, 1);
 
-    let visibleArr = visibleSlider(benefitBlock, 3, 2)[0];
-    let hiddenArr = visibleSlider(benefitBlock, 3, 2)[1];
+    const moveRight = (domElement) => {
+        //array of elements that are visible on the screen
+        let active = [...domElement].filter((n) => n.style.display === "block");
+        let visibleIndex = [...domElement].findIndex((n) => n.style.display === "block");
+        let nextIndex = visibleIndex + active.length;
 
-    //function for slider move click left
-    const leftMove = () => {
-        let itemOff = visibleArr.shift();
-        itemOff.style.display = "none";
-        hiddenArr.push(itemOff);
-        itemOff = hiddenArr.shift();
-        itemOff.style.display = "block";
-        visibleArr.push(itemOff);
-
-        return [visibleArr, hiddenArr];
+        if (nextIndex < domElement.length) {
+            active[0].style.display = "none";
+            domElement[nextIndex].style.display = "block";
+        } else {
+            return;
+        }
     };
 
-    //function for slider move click right
-    const rightMove = () => {
-        let itemOff = visibleArr.pop();
-        itemOff.style.display = "none";
-        hiddenArr.unshift(itemOff);
-        itemOff = hiddenArr.pop();
-        itemOff.style.display = "block";
-        visibleArr.unshift(itemOff);
-
-        return [visibleArr, hiddenArr];
+    const moveLeft = (domElement) => {
+        domElement = [...domElement].reverse();
+        moveRight(domElement);
     };
 
     body.addEventListener("click", (e) => {
         if (e.target.closest(".benefits__arrow")) {
             if (e.target.closest(".benefits__arrow--right")) {
-                console.log("right");
+                moveRight(benefitBlock);
             } else {
-                console.log("left");
+                moveLeft(benefitBlock);
+            }
+        }
+
+        if (e.target.closest(".services__arrow")) {
+            if (e.target.closest(".services__arrow--right")) {
+                moveRight(serviceBlock);
+            } else {
+                moveLeft(serviceBlock);
             }
         }
     });
